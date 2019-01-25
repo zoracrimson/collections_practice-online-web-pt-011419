74f8abe58a5057efb7a1ba1d1a1166f4fc8315bc
Kat Maldon <katharinamaldon@gmail.com>
2019-01-25 18:28:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,12 +7,12 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array[1], array[2] = array[2], array[1]
-  array
+  
 end
 
 def swap_elements(array, index)
-  
+  array[1], array[2] = array[2], array[1]
+  array
 end
 
 def reverse_array(array)
