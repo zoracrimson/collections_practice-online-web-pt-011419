0e62bb01a5bcd1f8cb641498975b7f119cedf4a6
Kat Maldon <katharinamaldon@gmail.com>
2019-01-25 17:46:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,13 +7,5 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array.sort do |a, b|
-  if a == b
-    0
-  elsif a < b
-    -1
-  elsif a > b
-    1
-  end
-end
+  array.sort_by {|x| x.length|}
 end
